2a29b3e73ff122a4bcc48d4d7c9eaa2c15403137
Cedric <cedric_deceuleneer@hotmail.com>
2013-01-25 20:28:54 +0100

    Avec objectifs

--- src/Launcher.java
+++ src/Launcher.java
@@ -8,5 +8,14 @@ public class Launcher {
 		// TODO Auto-generated method stub
 
 	}
-
+	/*Ajouter un livre, un exemplaire ->numero livre calculé sur base du genre, de l'auteur
+	  Rechercher un livre selon critère dont un mot dans le résumé / remarque
+	  Visualiser le taux d'emprunt, nb livre, nb exemplaire, % retard, % livre non récup
+	  Lister les livres prêté à un lecteur (passé et actuellement)
+	  Identifier les livres qui n'ont plus été empruntés depuis un certains temps
+	  Lister les livres selon leur état
+	  Lister les livres en retard
+	  Possibilité d'imprimer un listing en fct du temps
+	  Possibilité réaliser graphique en fct du temps
+	  */
 }
